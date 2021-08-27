7de7cba9843457713f1587dbee666ceb23ac2bd8
Long Hoàng <>
2021-08-27 07:49:59 +0700

    Updates

---
+++ precache-manifest.e9fc75cd4b876e67bb9e686d1b7adcd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23361e88351e8998b34be3724cd68b67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "073146845ad7f3b4e0db",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "acffc9a641b551cab9ed",
+    "url": "./static/js/2.a924aca0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a924aca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073146845ad7f3b4e0db",
+    "url": "./static/js/main.8a5e4d8a.chunk.js"
+  },
+  {
+    "revision": "0a7a89b21c9a63262836",
+    "url": "./static/js/runtime-main.7dbb4457.js"
+  }
+]);
